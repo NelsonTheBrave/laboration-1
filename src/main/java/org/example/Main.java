cba081c66c407becf045d6aa4731cbb0109ae6e4
Nelson <sherman.erik@gmail.com>
2024-09-05 08:46:20 +0200

    options 1-4 working, need better solution for 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,29 +8,37 @@ public class Main {
         PriceInputObject[] priceInput = new PriceInputObject[24];
         Scanner scanner = new Scanner(System.in);
         boolean menuActive = true;
+        boolean ratesEntered = false;
         while (menuActive) {
-            System.out.println("Elpriser");
+            System.out.println();
+            System.out.println("ELPRISER");
             System.out.println("========");
             System.out.println("1. Inmatning");
             System.out.println("2. Min, Max och Medel");
             System.out.println("3. Sortera");
             System.out.println("4. Bästa Laddningstid (4h)");
             System.out.println("e. Avsluta");
+            System.out.println();
+            System.out.print("Please enter your choice: ");
             try {
                 String menuInput = scanner.nextLine().toLowerCase();
                 switch (menuInput) {
                     case "1":
                         priceInput = priceInput();
+                        ratesEntered = true;
                         break;
                     case "2":
+                        if (ratesEntered) {
                         minMaxAverage(priceInput);
-                        break;
+                        break;} else {throw new NullPointerException();}
                     case "3":
+                    if (ratesEntered) {
                         sorting(priceInput);
-                        break;
+                        break;} else {throw new NullPointerException();}
                     case "4":
+                    if (ratesEntered) {
                         bestChargeTime(priceInput);
-                        break;
+                        break;} else {throw new NullPointerException();}
                     case "e":
                         System.out.println("Ending");
                         menuActive = false;
@@ -40,6 +48,9 @@ public class Main {
                 }
             } catch (IOException e) {
                 System.out.println("You have to enter a valid menu option - Please try again");
+            }
+            catch (NullPointerException e) {
+                System.out.println("Please enter the rates (option 1) before choosing any of the other options");
             }
         }
     }
@@ -62,6 +73,7 @@ public class Main {
             }
             hourlyRates[i] = new PriceInputObject(hourRange, rate);
         }
+
         return hourlyRates;
     }
 
@@ -71,7 +83,7 @@ public class Main {
         System.out.println("The highest rate(s) are:");
         for (int i = input.length - 1; i >= 0; i--) {
             if (input[i].getRate() == max) {
-                System.out.println(input[i]);
+                System.out.println("- " + input[i]);
             } else {
                 break;
             }
@@ -80,50 +92,44 @@ public class Main {
         System.out.println("The lowest rate(s) are:");
         for (int i = 0; i < 24; i++) {
             if (input[i].getRate() == min) {
-                System.out.println(input[i]);
+                System.out.println("- " + input[i]);
             } else {
                 break;
             }
         }
         OptionalDouble averageRate = Arrays.stream(input) // Calculating average rate and printing
-                .mapToInt(PriceInputObject::getRate)
-                .average();
-        System.out.println("The average rate for the whole 24 hrs is " + averageRate.getAsDouble() + " öre/kWh");
-        // TODO: Round it off to less decimals perhaps
+                .mapToInt(PriceInputObject::getRate).average();
+        if (averageRate.isPresent()) {
+            System.out.println("The average rate for the whole 24 hrs is " + (Math.round(averageRate.getAsDouble() * 10)) / 10.0 + " öre/kWh");
+        }
     }
 
     private static void sorting(PriceInputObject[] input) {
-        Arrays.sort(input, new RateComparator()); // Sorting array of classes according to rates
+        PriceInputObject[] sortedInput = Arrays.copyOf(input, input.length);
+        Arrays.sort(sortedInput, new RateComparator()); // Sorting array of classes according to rates
         System.out.println("The rates for the whole 24 hour period is as following, sorted from cheapest to most expensive:");
-        for (PriceInputObject priceInputObject : input) {
-            System.out.println(priceInputObject);
+        for (PriceInputObject priceInputObject : sortedInput) {
+            System.out.println("- " + priceInputObject);
         }
     }
 
     private static void bestChargeTime(PriceInputObject[] input) {
-        PriceInputObject[] rollingAverage = new PriceInputObject[20];
+        AverageRateList[] rollingAverage = new AverageRateList[21];
         int sumOfRates = 0;
-        int averageRates = 0;
+        double averageRates;
 
-        for (int i = 0; i < input.length - 4; i++) {
+        for (int i = 0; i < input.length - 3; i++) {
             for (int j = 0; j < 4; j++) {
                 sumOfRates += input[i + j].getRate();
             }
-            System.out.println("Sum of rates is: " + sumOfRates);
-            averageRates = sumOfRates / 4;
+            averageRates = sumOfRates / 4.00;
             String startingTime = input[i].getHourRange();
-            rollingAverage[i] = new PriceInputObject(startingTime, averageRates);
+            rollingAverage[i] = new AverageRateList(startingTime, averageRates);
             sumOfRates = 0;
         }
 
-        // Printing rolling shit
-        for (PriceInputObject priceInputObject : rollingAverage) {
-            System.out.println(priceInputObject);
-        }
-
-        Arrays.sort(rollingAverage, new RateComparator());
-        System.out.println("The best time to start charging your car is at: " + rollingAverage[0]);
-        // TODO: make a new class to present double? (or record)
+        Arrays.sort(rollingAverage, new AverageRateComparator());
+        System.out.println("The best time to start charging your car is at " + rollingAverage[0].getHourRange() + " o' clock at a an average rate of " + rollingAverage[0].getRate() + " öre/kWh");
     }
 }
 
@@ -152,6 +158,31 @@ class PriceInputObject {
     }
 }
 
+class AverageRateList {
+    String hourRange;
+    double rate;
+
+    // This is the constructor
+    AverageRateList(String hourRange, double rate) {
+        this.hourRange = hourRange;
+        this.rate = rate;
+    }
+
+    // Getters
+    public String getHourRange() {
+        return hourRange;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    @Override
+    public String toString() {
+        return rate + " öre/kWh at the time interval between " + hourRange + " o' clock";
+    }
+}
+
 class RateComparator implements Comparator<PriceInputObject> {
     @Override
     public int compare(PriceInputObject r1, PriceInputObject r2) {
@@ -159,8 +190,18 @@ class RateComparator implements Comparator<PriceInputObject> {
     }
 }
 
+class AverageRateComparator implements Comparator<AverageRateList> {
+    @Override
+    public int compare(AverageRateList dp1, AverageRateList dp2) {
+        return dp1.getRate().compareTo(dp2.getRate());
+    }
+}
+
 /*
 TODO:
  - Bug test, inputs and stuff
- - Write outputs in a nicer way from the PriceObject class, especially in option 4!
+ - Clean up code, split into separate documents?
+
+LEARNINGS:
+- Could I do it better than having two separate list classes (for int and double) and two dedicated comparators?
 */
